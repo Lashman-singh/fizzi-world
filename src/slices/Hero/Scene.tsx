101ddf0ca5105b3e49c29aff5d9518564ded7139
LashmanSingh <lashmansingh38034@gmail.com>
2025-02-25 21:56:10 -0600

    Added bublles animation in the hero section along with Loader and 3d models hidding for improving responsiveness for mobiles

--- src/slices/Hero/Scene.tsx
+++ src/slices/Hero/Scene.tsx
@@ -7,12 +7,15 @@ import { Group } from "three";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/all";
+import { useStore } from "@/hooks/useStore";
 
 type Props = {}
 
 gsap.registerPlugin(useGSAP, ScrollTrigger)
 
 export default function Scene({}: Props) {
+ 
+  const isReady = useStore((state) => state.isReady)
 
   const can1Ref = useRef<Group>(null)
   const can2Ref = useRef<Group>(null)
@@ -39,15 +42,19 @@ export default function Scene({}: Props) {
       !groupRef.current
     ) return;
 
-    gsap.set(can1Ref.current.position, {x: -1.5})
-    gsap.set(can1Ref.current.position, {z: -0.5})
+    isReady();
 
-    gsap.set(can2Ref.current.position, {x: 1.5})
-    gsap.set(can2Ref.current.position, {z: -0.5})
 
-    gsap.set(can3Ref.current.position, {y: 5, z:2})
-    gsap.set(can4Ref.current.position, {x: 2, y: 4, z:2})
-    gsap.set(can5Ref.current.position, {y: -5})
+    gsap.set(can1Ref.current.position, { x: -1.7 });
+    gsap.set(can1Ref.current.rotation, { z: -0.5 });
+
+    gsap.set(can2Ref.current.position, { x: 1.7 });
+    gsap.set(can2Ref.current.rotation, { z: 0.5 });
+
+    gsap.set(can3Ref.current.position, { y: 5, z: 2 });
+    gsap.set(can4Ref.current.position, { x: 2, y: 4, z: 2 });
+    gsap.set(can5Ref.current.position, { y: -5 });
+
 
     const introTl = gsap.timeline({
       defaults: {
@@ -56,12 +63,14 @@ export default function Scene({}: Props) {
       }
     })
 
+   if(window.scrollY < 20){
     introTl
     .from(Can1GroupRef.current.position, {y: -5, x: 1}, 0)
     .from(Can1GroupRef.current.position, {z: 3}, 0)
     .from(Can2GroupRef.current.position, {y: 5, x: 1}, 0)
     .from(Can2GroupRef.current.position, {z: 3}, 0)
 
+   }
     const scrollTl =gsap.timeline({
       defaults: {
         duration: 2,
@@ -74,31 +83,34 @@ export default function Scene({}: Props) {
       },
     })
 
-    scrollTl// Rotate can group
-    .to(groupRef.current.rotation, {y: Math.PI * 2}, 0)
+    scrollTl
+    // Rotate can group
+    .to(groupRef.current.rotation, { y: Math.PI * 2 })
 
-    //Can 1 - black cherry
-    .to(can1Ref.current.position, {X: -0.5, y: -0.7, z: -2}, 0)
-    .to(can1Ref.current.position, {z: 0.3}, 0)
+    // Can 1 - black cherry
+    .to(can1Ref.current.position, { x: -0.2, y: -0.7, z: -2 }, 0)
+    .to(can1Ref.current.rotation, { z: 0.3 }, 0)
 
-     //Can 2 - Lemone Lime
-     .to(can2Ref.current.position, {x: 1, y: -0.2, z: -1 }, 0)
-     .to(can2Ref.current.position, {z: 0}, 0)
+    // Can 2 - Lemon Lime
+    .to(can2Ref.current.position, { x: 1, y: -0.2, z: -1 }, 0)
+    .to(can2Ref.current.rotation, { z: 0 }, 0)
 
-      //Can 3 - Grape
-    .to(can3Ref.current.position, {x: -0.3, y: 0.5, z: -1}, 0)
-    .to(can3Ref.current.position, {z: -0.1}, 0)
+    // Can 3 - Grape
+    .to(can3Ref.current.position, { x: -0.3, y: 0.5, z: -1 }, 0)
+    .to(can3Ref.current.rotation, { z: -0.1 }, 0)
 
-     //Can 1 - Strawberry
-    .to(can4Ref.current.position, {x: 0, y: -0.3, z: 0.5}, 0)
-    .to(can4Ref.current.position, {z: 0.3}, 0)
+    // Can 4 - Strawberry Lemonade
+    .to(can4Ref.current.position, { x: 0, y: -0.3, z: 0.5 }, 0)
+    .to(can4Ref.current.rotation, { z: 0.3 }, 0)
 
-      //Can 1 - Watermelon
-    .to(can5Ref.current.position, {x: 0.3, y: 0.5, z: -0.5}, 0)
-    .to(can5Ref.current.position, {z: -0.25}, 0)
-
-    .to(groupRef.current.position, {x: 1, duration: 3, ease: "sine.inOut"}, 1.3)
-
+    // Can 5 -Watermelon
+    .to(can5Ref.current.position, { x: 0.3, y: 0.5, z: -0.5 }, 0)
+    .to(can5Ref.current.rotation, { z: -0.25 }, 0)
+    .to(
+      groupRef.current.position,
+      { x: 1, duration: 3, ease: "sine.inOut" },
+      1.3,
+    );
 
   })
   return (
